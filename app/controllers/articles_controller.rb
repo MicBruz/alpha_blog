a506d69b3a5d29b4e4ec48b2ecdd9d743bbbf944
Michal Bru <mbruzda1@wp.pl>
2021-02-12 19:05:28 +0100

    158 Controller methds as helper methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params) #whitelisting of title and description
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
